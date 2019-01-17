cb794893725adf20c8f4a8461cfcb6b15952f9fa
username@email.com <354037968@qq.com>
2019-01-17 17:06:07 +0800

    初始化算法库

---
+++ src/calculation/to_sum.go
@@ -0,0 +1,20 @@
+package calculation
+
+//思路：判断数组中所有值得组合方式，是否满足条件
+
+func TwoSum(nums []int, target int) []int {
+
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if (nums[i] + nums[j]) == target {
+				return []int{i, j}
+			}
+		}
+	}
+
+	return []int{}
+}
+
+//测试
+//TwoSum([]int{3, 2, 3}, 6)
+//[0,2]
