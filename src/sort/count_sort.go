f91c3d08800e825e2efead06c397c38a5527003b
hyb <354037968@qq.com>
2019-03-14 23:24:52 +0800

    完成计数排序

--- src/sort/count_sort.go
+++ src/sort/count_sort.go
@@ -2,10 +2,18 @@ package sort
 
 // 计数排序
 /*
-   计算数组中最大值
-   申请一个临时最大值加1长度的数组,用来统计数出现的次数,因为有0存在所以加1
-   循环计算每一位出现的次数,如果出现了就+1
-   通过遍历计算临时存储数组值放入到最终数据
+   @data	  []int{2,5,3,0,2,3,0,3}
+   @for  	  计算数组中最大值
+   @dataTemp  申请一个临时最大值加1长度的数组,用来统计数出现的次数,因为有0存在所以加1
+   		 	  循环计算每一位出现的次数,如果出现了就+1,通过遍历计算临时存储数组值放入到最终数据
+			  []int{2,0,2,3,0,1} 例对应：0有2个,1有0个..
+   @dataTemp  把数组值从左往右以此相加  []int{2, 2 ,4 ,7 ,7 ,8}
+			  注：2代表小于等于0的数据，4小于等于2的数
+   @result	  []int{2,5,3,0,2,3,0,3}  []int{2, 2 ,4 ,7 ,7 ,8}
+			  dataTemp[data[i]] - 1 通过原始数据值，找到桶中对应值得位置,-1是因为数组从0开始
+			  result[index] = data[i] 根据位置存放取出的值
+			  dataTemp[data[i]]--	桶中被取走了一个值，所以位置减去一个
+				例：存在相同值 3和3的排序
 */
 
 func CountSort(data []int) []int {
@@ -22,22 +30,28 @@ func CountSort(data []int) []int {
 		}
 	}
 	
-	var dataTemp []int = make([]int, len(data)+1)
+	// @todo 分配桶
+	var dataTemp []int = make([]int, max+1)
 	
 	for i := 0; i < len(data); i++ {
 		dataTemp[data[i]]++
 	}
 	
-	var r []int =make([]int,max+1)
-	for i := len(data)-1; i >=0; i-- {
-		index :=dataTemp[data[i]]-1
-		r[index] = data[i]
+	for i := 1; i <= max; i++ {
+		dataTemp[i] = dataTemp[i-1] + dataTemp[i]
+	}
+	
+	var result []int = make([]int, len(data))
+	
+	for i := len(data) - 1; i >= 0; i-- {
+		
+		index := dataTemp[data[i]] - 1
+		
+		result[index] = data[i]
+		
 		dataTemp[data[i]]--
+		
 	}
 	
-	for i := 0; i < len(data); i++ {
-		data[i] = r[i]
-	}
-	
-	return data
+	return result
 }
--- tests/sort/count_test.go
+++ tests/sort/count_test.go
@@ -9,7 +9,7 @@ import (
 // @test count sort
 func TestCountSort(t *testing.T) {
 	
-	result := sort.CountSort([]int{0, 0, 2, 2, 3, 3, 3, 5})
+	result := sort.CountSort([]int{2,3,0,2,3,0,3,5})
 	
 	assert.Equal(t, result, []int{0,0,2,2,3,3,3,5}, " quick sort error")
 }
