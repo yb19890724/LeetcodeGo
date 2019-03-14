f31225cc96499938e7c1e1e6e183c0653a9deeb9
hyb <354037968@qq.com>
2019-03-15 00:00:12 +0800

    最终提交

--- tests/sort/quick_test.go
+++ tests/sort/quick_test.go
@@ -8,7 +8,7 @@ import (
 
 // @test quick sort
 func TestQuickSort(t *testing.T) {
-
+	
 	result := sort.QuickSort([]int{1, 34, 6, 7, 10})
 	
 	assert.Equal(t, result, []int{1, 6, 7, 10, 34}, " data quick sort error. ")
