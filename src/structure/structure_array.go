709cc1192a25e43e46ba70c38d84bc7ea22d04ee
hyb <354037968@qq.com>
2019-03-21 22:58:27 +0800

    完成稀疏数组测试

--- src/structure/structure_array.go
+++ src/structure/structure_array.go
@@ -1,79 +1,59 @@
-package main
-
-import (
-	`fmt`
-)
-
-// 默认棋局大小
-// var chessMap [][]int
+package structure
 
 // 值节点
 type ValNode struct {
-	row int
-	col int
-	val int
+	Row int // 记录行
+	Col int // 记录列
+	Val int // 记录值
 }
 
-// 打印棋局
-func ShowChessMap(chessMap [][]int) {
-	// 输出看原始数组
+// 创建记录数据范围值
+
+func CreateArray(row int, col int) [][]int {
+	var data = make([][]int, row, col)
 	
-	for _, v := range chessMap {
-		
-		for _, v2 := range v {
-			
-			fmt.Printf("%d\t", v2)
-		}
-		
-		fmt.Println()
+	for i := 0; i < col; i++ {
+		var rowArray []int = make([]int, row, col)
+		data[i] = rowArray
 	}
-	
+	return data
 }
 
-// 恢复棋盘
-func LoadGame(sparseArr []ValNode) [][]int {
+// 恢复原始数据
+// @var sparseArr[0]  	记录行，记录记录列
+// @var sparseArr[0:]   记录值
+
+func RecoverSparseArr(sparseArr []ValNode) [][]int {
 	
-	chessboardLayout := sparseArr[0]  // 获取棋盘的范围
-	sparseArr = sparseArr[0:] // 去掉棋盘的大小
+	size, sparseArr := sparseArr[0], sparseArr[0:]
 	
-	chessMap := CreateChessMap(chessboardLayout.row, chessboardLayout.col)
+	data := CreateArray(size.Row, size.Col)
 	
 	// 便利 每一行文件
 	for i, valNode := range sparseArr {
 		
 		if i != 0 { // 这里判断是因为兼容 第一个存储的是规模
 			
-			chessMap[valNode.row][valNode.col] = valNode.val
+			data[valNode.Row][valNode.Col] = valNode.Val
 		}
 		
 	}
-	return chessMap
+	return data
 }
 
-// 创建空棋局
-func CreateChessMap(row int, col int) [][]int {
-	var chessMap = make([][]int, row, col)
-	
-	for i := 0; i < col; i++ {
-		var rowArray []int = make([]int, row, col)
-		chessMap[i] = rowArray
-	}
-	return chessMap
-}
-
-// 存储棋局
-func SaveChessMap(chessMap [][]int, sparseArr []ValNode) []ValNode {
+// 存储记录到稀疏数组
+func SaveSparseArr(data [][]int, sparseArr []ValNode) []ValNode {
 	
 	// 存储稀疏数组值
-	for i, v := range chessMap {
+	for i, v := range data {
 		
 		for j, v2 := range v {
 			
 			if v2 != 0 {
 				var valNode = ValNode{
-					row: i,
-					col: j,
-					val: v2,
+					Row: i,
+					Col: j,
+					Val: v2,
 				}
 				sparseArr = append(sparseArr, valNode)
 			}
@@ -81,46 +61,4 @@ func SaveChessMap(chessMap [][]int, sparseArr []ValNode) []ValNode {
 		
 	}
 	return sparseArr
-}
-
-func main() {
-	
-	row, col := 11, 11 // 定义棋局范围
-	chessMap := CreateChessMap(row, col)
-	
-	chessMap[1][2] = 1 // 黑棋子
-	chessMap[2][3] = 2 // 白棋子
-	
-	ShowChessMap(chessMap)
-	
-	// 创建一个值节点
-	var sparseArr []ValNode
-	
-	// 标准稀疏数组应该有，原始数据的规模（行和列，默认值）
-	valNode := ValNode{
-		row: 11,
-		col: 11,
-		val: 0,
-	}
-	sparseArr = append(sparseArr, valNode)
-	
-	// 存储棋局和棋盘
-	saveChessMap := SaveChessMap(chessMap, sparseArr)
-	
-	
-	// 查看存储的棋局
-	for i, valNode := range saveChessMap {
-	 	fmt.Printf("%d: %d %d %d\n", i, valNode.row, valNode.col, valNode.val)
-	}
-	
-	// 读取进度
-	loadGame := LoadGame(saveChessMap)
-	
-	// 读取棋盘进度
-	for _, v := range loadGame {
-		for _, v2 := range v {
-			fmt.Printf("%d\t", v2)
-		}
-		fmt.Println()
-	}
 }
---
+++ tests/structure/structure_array_test.go
@@ -0,0 +1,163 @@
+package structure
+
+import (
+	"fmt"
+	"github.com/hyb/LeetCode/src/structure"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+// 打印棋局
+func ShowChessMap(data [][]int) {
+	// 输出看原始数组
+	
+	for _, v := range data {
+		
+		for _, v2 := range v {
+			
+			fmt.Printf("%d\t", v2)
+		}
+		
+		fmt.Println()
+	}
+	
+}
+
+// @todo: 创建记录行测试数据
+
+func createTestData(row int, col int) ([]int, []int, []int) {
+	
+	var defaultRow = make([]int, row, col)
+	
+	var twoRow, threeRow = make([]int, row, col), make([]int, row, col)
+	
+	twoRow[2], threeRow[3] = 1, 2
+	
+	return defaultRow, twoRow, threeRow
+}
+
+// @todo 创建棋局数据，模拟棋局数据
+
+func createChessMapData(row int, col int) [][]int {
+	
+	chessMap := createArray(row, col)
+	
+	chessMap[1][2], chessMap[2][3] = 1, 2
+	
+	return chessMap
+}
+
+// @todo 创建数组
+func createArray(row int, col int) [][]int {
+	
+	chessMap := structure.CreateArray(row, col)
+	
+	return chessMap
+}
+
+// @todo 创建稀疏数组记录
+func createSparseArray(row int, col int) []structure.ValNode {
+	
+	sparseArray := []structure.ValNode{
+		{
+			Row: row,
+			Col: col,
+			Val: 0,
+		},
+		{
+			Row: 1,
+			Col: 2,
+			Val: 1,
+		},
+		{
+			Row: 2,
+			Col: 3,
+			Val: 2,
+		},
+	}
+	return sparseArray
+	
+}
+
+// @todo 测试创建棋局
+
+func TestCreateSparseArray(t *testing.T) {
+	
+	row, col := 11, 11 // 定义棋局范围
+	
+	chessMap := createChessMapData(row, col)
+	
+	defaultRow, twoRow, threeRow := createTestData(row, col)
+	
+	for index, v := range chessMap {
+		
+		if 1 == index {
+			
+			assert.Equal(t, v, twoRow, " default row comparative failure ")
+			continue
+		}
+		
+		if 2 == index {
+			
+			assert.Equal(t, v, threeRow, " two row comparative failure")
+			continue
+		}
+		
+		assert.Equal(t, v, defaultRow, " two row comparative failure")
+	}
+	
+}
+
+// @todo 存储棋局
+func TestSaveSparseArr(t *testing.T) {
+	
+	row, col := 11, 11 // 定义棋局范围
+	
+	chessMap := createChessMapData(row, col)
+	
+	var sparseArr []structure.ValNode
+	
+	valNode := structure.ValNode{
+		Row: row,
+		Col: col,
+		Val: 0,
+	}
+	sparseArr = append(sparseArr, valNode)
+	
+	saveChessMap := structure.SaveSparseArr(chessMap, sparseArr)
+	
+	actualChessMap := createSparseArray(row, col)
+	
+	assert.Equal(t, actualChessMap, saveChessMap, " save sparse array error ")
+	
+}
+
+// @todo 恢复棋局
+func TestRecoverSparseArr(t *testing.T) {
+	
+	row, col := 11, 11 // 定义棋局范围
+	
+	recodeChessMap := createSparseArray(row, col)
+	
+	actualChessMap := structure.RecoverSparseArr(recodeChessMap)
+	
+	defaultRow, twoRow, threeRow := createTestData(row, col)
+	
+	for index, v := range actualChessMap {
+		
+		if 1 == index {
+			
+			assert.Equal(t, v, twoRow, " default row comparative failure ")
+			continue
+		}
+		
+		if 2 == index {
+			
+			assert.Equal(t, v, threeRow, " two row comparative failure")
+			continue
+		}
+		
+		assert.Equal(t, v, defaultRow, " two row comparative failure")
+		
+	}
+}
