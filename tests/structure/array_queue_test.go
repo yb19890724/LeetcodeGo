2c9b6f45b28bdb6afdd25f18e15217ee76253342
hyb <354037968@qq.com>
2019-03-24 21:04:19 +0800

    更新顺序队列

---
+++ tests/structure/array_queue_test.go
@@ -0,0 +1,56 @@
+package structure
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/yb19890724/leetcode-go/src/structure"
+	"testing"
+)
+
+// 初始化公用队列结构
+func initQueue() *structure.ArrayQueue {
+	arrayQueue := &structure.ArrayQueue{
+		5,
+		[5]int{},
+		0,
+		0,
+	}
+	
+	arrayQueue.Enqueue(1)
+	arrayQueue.Enqueue(2)
+	arrayQueue.Enqueue(3)
+	arrayQueue.Enqueue(4)
+	arrayQueue.Enqueue(5)
+	
+	return arrayQueue
+}
+
+// @test  queue enqueue 测试入列
+func TestDequeue(t *testing.T) {
+	
+	arrayQueue := initQueue()
+	
+	assert.Equal(t, arrayQueue.All(), [5]int{1, 2, 3, 4, 5}, " queue enqueue error ")
+	
+}
+
+// @test  queue dequeue 测试出队
+func TestEnQueue(t *testing.T) {
+	
+	arrayQueue := initQueue()
+	
+	testData := []int{1, 2, 3, 4, 5}
+	
+	for i := 0; i < arrayQueue.MaxSize; i++ {
+		assert.Equal(t, arrayQueue.Dequeue(), testData, " queue dequeue error ")
+	}
+	
+}
+
+// @test queue max size 测试队列长度限制
+func TestQueueMaxSize(t *testing.T) {
+	
+	arrayQueue := initQueue()
+	
+	assert.Equal(t, arrayQueue.Enqueue(6), false, " queue max size overstep ")
+	
+}
