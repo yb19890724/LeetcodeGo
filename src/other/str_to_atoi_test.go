c7f9cc90bd97f6833e830bb8dac1668fe9c426f0
hyb <354037968@qq.com>
2020-03-29 14:49:13 +0800

    删除注释

--- src/other/str_to_atoi_test.go
+++ src/other/str_to_atoi_test.go
@@ -1,9 +1,7 @@
 package other
 
 import (
-	"fmt"
 	"github.com/stretchr/testify/assert"
-	"reflect"
 	
 	"testing"
 )
@@ -14,20 +12,5 @@ func TestMyAtoi(t *testing.T) {
 	assert.Equal(t, MyAtoi("42"), 42, " atoi error")
 	assert.Equal(t, MyAtoi("   -42"), -42, " atoi error")
 	assert.Equal(t, MyAtoi("words and 987"), 0, " atoi error")
-	
-	s :="0"
-	
-	fmt.Println(0x10) // 10进制
-	fmt.Println(string('0')) // 10进制
-	fmt.Println(int(s[0]-'0')) // 10进制
-	
-	
-	
-	fmt.Println(reflect.TypeOf(s[0]))
-	fmt.Println(reflect.TypeOf(int(s[0])))
-	fmt.Println(reflect.TypeOf('0'))
-	fmt.Println(reflect.TypeOf(int(s[0])-'0'),s[0],'0',int(s[0])-'0')
-	
-	
 	
 }
